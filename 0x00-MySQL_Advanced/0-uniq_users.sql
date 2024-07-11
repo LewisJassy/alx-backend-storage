8d6274f6aa801c306bb6059a63625083c2a0d462
LewisJassy <jassylewis70@gmail.com>
2024-07-11 23:11:06 +0300

    solve the syntax error

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- create a database named users with the following columns
-DROP TABLES IF EXISTS users;
-CREATE DATABASE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
